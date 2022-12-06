7a9012acdab642ba506e7fd2e0f597e544076997
Jorgpro <115546930+Jorgpro@users.noreply.github.com>
2022-12-05 22:00:29 -0300

    Create numerosJS.js

---
+++ numerosJS.js
@@ -0,0 +1,10 @@
+let alturaCentimetros= 1.850 ;
+let alturaMetros=1.85;
+let pesoKg= 85;
+let altura= 1.80;
+
+let max_valor_js= Number.MAX_VALUE;
+console.log(max_valor_js + 1);
+console.log(max_valor_js )
+let max_valueMas1=1.7976931348623157e+308
+let max_value=1.7976931348623157e+308
